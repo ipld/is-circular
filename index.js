de02f6af9606c4fedee744982843f11df7ed68d0
Irakli Gozalishvili <contact@gozala.io>
2020-09-16 09:43:23 -0700

    feat: add ignore list for allowing recursion

--- index.js
+++ index.js
@@ -4,28 +4,34 @@ module.exports = isCircular
 
 /**
  * checks whether the object is circular
- * @param  {object}  obj - object to check circularity for
+ * @param {object}  obj - object to check circularity for
+ * @param {Object<string, true>} ignore - dictionary who's keys will be ignored
  * @return {Boolean} true if obj is circular, false if it is not
  */
-function isCircular (obj) {
+function isCircular (obj, ignore={}) {
   if (!(obj instanceof Object)) {
     throw new TypeError('"obj" must be an object (or inherit from it)')
   }
-  return _isCircular(obj)
+  return _isCircular(obj, ignore)
 }
 
 /**
  * @private
  * checks whether the object is circular
- * @param  {object}  obj - object to check circularity for
- * @param  {Node}    parentList - linked-list that contains all the object's parents
+ * @param {object}  obj - object to check circularity for
+ * @param {Object<string, true>} ignore - dictionary who's keys will be ignored
+ * @param {Node}    parentList - linked-list that contains all the object's parents
  * @return {Boolean} true if obj is circular, false if it is not
  */
-function _isCircular (obj, parentList) {
+function _isCircular (obj, ignore, parentList) {
   parentList = new Node(obj, parentList)
 
   // breadth-first search for circular object
   for (var key in obj) {
+    if (ignore[key]) {
+      continue
+    }
+
     var val = obj[key]
     if (val instanceof Object) {
       if (parentList.contains(val) || _isCircular(val, parentList)) {
